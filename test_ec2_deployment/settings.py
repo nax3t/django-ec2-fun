1b00b6b7b1be37f6604dbd2dc3e74cbd045f13ce
Ian Schoonover <learntocodeinfo@gmail.com>
2024-05-02 13:56:06 -0500

    Update host ip

--- test_ec2_deployment/settings.py
+++ test_ec2_deployment/settings.py
@@ -25,7 +25,7 @@ SECRET_KEY = 'django-insecure-kq4yvk_xrm699q^43i332y^do3cpaq!-i*m@wl^)eg!oa*lio2
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = ['3.145.78.187']
+ALLOWED_HOSTS = ['127.0.0.1', '3.137.222.47']
 
 
 # Application definition
